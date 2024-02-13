9307021e3339c34a034e53d5b47d15031ef66b89
김현빈 <gusqls81017@gmail.com>
2024-02-13 09:39:44 +0900

    d

---
+++ src/test/java/com/team5/projrental/payment/review/PaymentReviewMapperTest.java
@@ -0,0 +1,60 @@
+package com.team5.projrental.payment.review;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentReviewMapperTest {
+
+    @Test
+    void insReview() {
+    }
+
+    @Test
+    void selUpProIs() {
+    }
+
+    @Test
+    void upProductIstatus() {
+    }
+
+    @Test
+    void upReview() {
+    }
+
+    @Test
+    void delReview() {
+    }
+
+    @Test
+    void selReIstatus() {
+    }
+
+    @Test
+    void selReview() {
+    }
+
+    @Test
+    void selBuyRew() {
+    }
+
+    @Test
+    void selPatchRev() {
+    }
+
+    @Test
+    void selUser() {
+    }
+
+    @Test
+    void selRat() {
+    }
+
+    @Test
+    void upRating() {
+    }
+
+    @Test
+    void sleDelBefor() {
+    }
+}
